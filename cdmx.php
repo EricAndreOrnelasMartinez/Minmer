7295d7a3e7e3d81d0d16daf8d236ba4e881854a3
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-05 21:17:51 -0600

    add

--- cdmx.php
+++ cdmx.php
@@ -1,7 +1,12 @@
 <?php 
 
+include('cndb.php');
+
 $userN = $_POST['username'];
 $pass = $_POST['pass'];
+$sql = 'SELECT Contrasena FROM Users WHERE Nombre="'.$userN;
+$ans = mysqli_query($con,$sql);
+echo $ans;
 
 
 
