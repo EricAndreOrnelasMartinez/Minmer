6c59880ec288d6b49aee009ac23fe931c13e7b4b
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-05 22:21:06 -0600

    add

--- cdmx.php
+++ cdmx.php
@@ -8,8 +8,15 @@ $sql = 'SELECT Contrasena FROM Users WHERE Nombre="'.$userN;
 $ans = mysqli_query($con,$sql);
 echo $ans;
 
-
-
-
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
