a55bc9c2d490be9f734022ab5af3ecef08a228c9
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-06 20:19:59 -0600

    add

--- cdmx.php
+++ cdmx.php
@@ -1,21 +1,19 @@
 <?php 
 error_reporting(E_ALL);
 ini_set('display_errors','1');
-echo "todo bien";
 include('cndb.php');
-echo "todo bien 2";
 $userN = $_POST['username'];
 $pass = $_POST['pass'];
 $sql = 'SELECT Contrasena FROM Users WHERE Nombre="'.$userN.'";';
 $ans = mysqli_query($con,$sql);
 $returnedV = validation($ans, $pass);
+echo "valor: ";
 echo $returnedV;
 //if(validation($ans, $pass)){
 //    echo "Usuario valido";
 //}else{
 //    echo "Usuario invalido";
 //}
-echo "todo bien 3";
 ?>
 <!DOCTYPE html>
 <html lang="en">
