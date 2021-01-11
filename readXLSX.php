4b9eff40863a30da752e45f586538eae59079ede
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-10 18:44:41 -0600

    add

--- readXLSX.php
+++ readXLSX.php
@@ -112,7 +112,7 @@ function readAndGDL($fileU){
         }else{
             echo "algo falló";
         }
-        if($i === $nRows){
+        if($i === $nRows1){
             $isfished1 = true;
         }
     }
