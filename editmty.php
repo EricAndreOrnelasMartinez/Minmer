5d90b2825fa8ecc948cf03e071bbaf2bf048710c
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-10 19:10:25 -0600

    add

---
+++ editmty.php
@@ -0,0 +1,101 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors','1');
+
+session_start();
+
+if(isset($_SESSION['user'])){
+}else{
+    header("Location:index.php");
+}
+function hasA($string){
+    $prove = false;//explode
+    $arr = explode(" ",$string);
+    foreach($arr as $indexL){
+        if($indexL === "a" || $indexL === "A"){
+            $prove = true;
+            break;
+        }
+    }
+    return $prove;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="mty.php" method="post">
+    <?php
+    $con = mysqli_connect("localhost","root","Lasric.2018","Minmer");
+    $tem_id = round($_GET['ids']);
+    echo $tem_id;
+    $sqlFC = "SELECT FechaC FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultFC = mysqli_query($con,$sqlFC);
+    $sqlFE = "SELECT FechaE FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultFE = mysqli_query($con,$sqlFE);
+    $sqlOper = "SELECT Operador FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultOper = mysqli_query($con,$sqlOper);
+    $sqlPlac = "SELECT Placas FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultPlac = mysqli_query($con,$sqlPlac);
+    $sqlID = "SELECT ID FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultID = mysqli_query($con,$sqlID);
+    $sqlOS = "SELECT OS FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultOS = mysqli_query($con,$sqlOS);
+    $sqlFact = "SELECT Factura FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultFact = mysqli_query($con,$sqlFact);
+    $sqlCli = "SELECT Cliente FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultCli = mysqli_query($con,$sqlCli);
+    $sqlPZS = "SELECT PZS FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultPZS = mysqli_query($con,$sqlPZS);
+    $sqlCaja = "SELECT Caja FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultCaja = mysqli_query($con,$sqlCaja);
+    $sqlSub =  "SELECT Subtotal FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultSub = mysqli_query($con,$sqlSub);
+    $sqlHor = "SELECT Horario FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultHor = mysqli_query($con,$sqlHor);
+    $sqlDire = "SELECT Direccion FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultDire = mysqli_query($con,$sqlDire);
+    $sqlDest =  "SELECT Destino FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultDest = mysqli_query($con,$sqlDest);
+    $sqlConce = "SELECT Concepto FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultConce = mysqli_query($con,$sqlConce);
+    $sqlCapa = "SELECT Capacidad FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultCapa = mysqli_query($con,$sqlCapa);
+    $sqlObser = "SELECT Observaciones FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultObser =  mysqli_query($con,$sqlObser);
+    $sqlOE = "SELECT OE FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultOE = mysqli_query($con,$sqlOE);
+    $sqlCust = "SELECT Custodia FROM MTY WHERE ID_SQL=".$tem_id.";";
+    $resultCust = mysqli_query($con,$sqlCust);
+    ?>
+    Fecha de carga: <input type="text" name="FechaC"  value="<?php echo implode(mysqli_fetch_assoc($resultFC)); ?>"><br>
+    Fecha de entrega: <input type="text" name="FechaE"  value="<?php echo implode(mysqli_fetch_assoc($resultFE)); ?>"><br>
+    Operador: <input type="text" name="Operador" value="<?php echo implode(mysqli_fetch_assoc($resultOper)); ?>"><br>
+    Placas: <input type="text" name="Placas"  value="<?php echo implode(mysqli_fetch_assoc($resultPlac)); ?>"><br>
+    ID: <input type="text" name="ID" value="<?php echo implode(mysqli_fetch_assoc($resultID)); ?>"><br>
+    SO: <input type="text" name="OS"  value="<?php echo implode(mysqli_fetch_assoc($resultOS)); ?>"><br>
+    Factura: <input type="text" name="Factura"  value="<?php echo implode(mysqli_fetch_assoc($resultFact)); ?>"><br>
+    Cliente: <input type="text" name="Cliente"  value="<?php echo implode(mysqli_fetch_assoc($resultCli)); ?>"><br>
+    PZS: <input type="text" name="PZS"  value="<?php echo implode(mysqli_fetch_assoc($resultPZS)); ?>"><br>
+    Cajas: <input type="text" name="Caja"  value="<?php echo implode(mysqli_fetch_assoc($resultCaja)); ?>"><br>
+    Subtotal: <input type="text" name="Subtotal"  value="<?php echo implode(mysqli_fetch_assoc($resultSub)); ?>"><br>
+    Horario: <input type="text" name="Horario"  value="<?php echo implode(mysqli_fetch_assoc($resultHor)); ?>">:00<br>
+    Direccion: <input type="text" name="Direccion"  value="<?php echo implode(mysqli_fetch_assoc($resultDire)); ?>"><br>
+    Destino: <input type="text" name="Destino"  value="<?php echo implode(mysqli_fetch_assoc($resultDest)); ?>"><br>
+    Concepto: <input type="text" name="Concepto"  value="<?php echo implode(mysqli_fetch_assoc($resultConce)); ?>"><br>
+    Capacidad: <input type="text" name="Capacidad"  value="<?php echo implode(mysqli_fetch_assoc($resultCapa)); ?>"><br>
+    Observaciones: <input type="text" name="Observaciones"  value="<?php echo implode(mysqli_fetch_assoc($resultObser)); ?>"><br>
+    OE: <input type="text" name="OE"  value="<?php echo implode(mysqli_fetch_assoc($resultOE)); ?>"><br>
+    Custodia: <input type="text" name="Custodia"  value="<?php echo implode(mysqli_fetch_assoc($resultCust)); ?>"><br>
+    <input type="hidden" name="ID_SQL" value="<?php echo $tem_id;  ?>">
+    <input type="submit" value="Guardar">
+    </form>
+    <?php $con->close(); ?>
+</body>
+</html>
--- readXLSX.php
+++ readXLSX.php
@@ -117,7 +117,61 @@ function readAndGDL($fileU){
         }
     }
     if($isfished1){
+        readAndMTY($fileU);
         $con1->close();
+    }
+    }
+
+function readAndMTY($fileU){
+    $con1 = mysqli_connect("localhost","root","Lasric.2018","Minmer");
+    $fileName1 = __DIR__."/uploads/".$fileU;
+    $obReader1 = PHPExcel_IOFactory::load($fileName1); 
+    $obReader1->setActiveSheetIndex(2);
+    $nRows1 = $obReader1->setActiveSheetIndex(2)->getHighestRow();
+    $isfished1 = false;
+    $HorarioT1 = "Error";
+    for($i = 2; $i <= $nRows1; $i++){
+        $FechaC1 = $obReader1->getActiveSheet()->getCell('A'.$i)->getCalculatedValue();
+        $FechaE1 = $obReader1->getActiveSheet()->getCell('B'.$i)->getCalculatedValue();
+        $Operador1 = $obReader1->getActiveSheet()->getCell('C'.$i)->getCalculatedValue();
+        $Placas1 = $obReader1->getActiveSheet()->getCell('D'.$i)->getCalculatedValue();
+        $ID1 = $obReader1->getActiveSheet()->getCell('E'.$i)->getCalculatedValue();
+        $SO1 = $obReader1->getActiveSheet()->getCell('F'.$i)->getCalculatedValue();
+        $Factura1 = $obReader1->getActiveSheet()->getCell('G'.$i)->getCalculatedValue();
+        $Cliente1 = $obReader1->getActiveSheet()->getCell('H'.$i)->getCalculatedValue();
+        $PZS1 = $obReader1->getActiveSheet()->getCell('I'.$i)->getCalculatedValue();
+        $Cajas1 = round($obReader1->getActiveSheet()->getCell('J'.$i)->getCalculatedValue());
+        $Subtotal1 = $obReader1->getActiveSheet()->getCell('K'.$i)->getCalculatedValue();
+        $Horario1 = $obReader1->getActiveSheet()->getCell('L'.$i)->getCalculatedValue();
+        if(!empty($Horario1)){
+            if(!hasAA($Horario1)){
+                $HorarioT1 = $Horario1 * 24;
+            }else {
+                $HorarioT1 = $Horario1;
+            }
+            }else{
+            $HorarioT1 = "PENDIENTE";
+            }
+        $Direccion1 = $obReader1->getActiveSheet()->getCell('M'.$i)->getCalculatedValue();
+        $Destino1 = $obReader1->getActiveSheet()->getCell('N'.$i)->getCalculatedValue();
+        $Concepto1 = $obReader1->getActiveSheet()->getCell('O'.$i)->getCalculatedValue();
+        $Capacidad1 = $obReader1->getActiveSheet()->getCell('P'.$i)->getCalculatedValue();
+        $Observaciones1 = $obReader1->getActiveSheet()->getCell('Q'.$i)->getCalculatedValue();
+        $OE1 = $obReader1->getActiveSheet()->getCell('R'.$i)->getCalculatedValue();
+        $Custodia1 = $obReader1->getActiveSheet()->getCell('S'.$i)->getCalculatedValue();
+        $sql1 = "INSERT INTO GDL(FechaC,FechaE,Operador,Placas,ID,OS,Factura,Cliente,PZS,Caja,Subtotal,Horario,Direccion,Destino,Concepto,Capacidad,Observaciones,OE,Custodia) VALUES('$FechaC1','$FechaE1','$Operador1','$Placas1','$ID1','$SO1','$Factura1','$Cliente1','$PZS1','$Cajas1','$Subtotal1','$HorarioT1','$Direccion1','$Destino1','$Concepto1','$Capacidad1','$Observaciones1','$OE1','$Custodia1');";
+        $rmysql1 = mysqli_query($con1, $sql1);
+        if($rmysql1){
+            echo "capturado!!";
+        }else{
+            echo "algo falló";
+        }
+        if($i === $nRows1){
+            $isfished1 = true;
+        }
+        if($isfished1){
+            $con1->close();
+        }
     }
     }
 
