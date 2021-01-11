03899086a5ff3d19b4a75de9cdcb84c8a1bb4ca1
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-11 13:20:15 -0600

    add

--- sjd.php
+++ sjd.php
@@ -18,6 +18,7 @@ if(isset($_SESSION['user'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cssforT.css">
     <title>SJD</title>
 </head>
 <body>
