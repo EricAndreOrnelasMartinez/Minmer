2fbefb7c1f934b552bc24e13f7f7b1f534b1fa3f
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-11 13:30:37 -0600

    add

--- cun.php
+++ cun.php
@@ -18,7 +18,7 @@ if(isset($_SESSION['user'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="cssforTs.css">
+    <link rel="stylesheet" href="cssforT.css">
     <title>CUN</title>
 </head>
 <body>
