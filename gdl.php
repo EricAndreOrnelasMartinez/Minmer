235a901c5482579bce6c3321c9bd7daa95308d6a
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-10 21:12:36 -0600

    add

--- gdl.php
+++ gdl.php
@@ -116,12 +116,12 @@ if(isset($_POST['FechaC'])){
     $sqlUpdate = "UPDATE GDL SET FechaC='$FechaC1',FechaE='$FechaE1',Operador='$Operador1',Placas='$Placas1',ID='$ID1',OS='$SO1',Factura='$Factura1',Cliente='$Cliente1',PZS='$PZS1',Caja='$Cajas1',Subtotal='$Subtotal1',Horario='$Horario1',Direccion='$Direccion1',Destino='$Destino1',Concepto='$Concepto1',Capacidad='$Capacidad1',Observaciones='$Observaciones1',OE='$OE1',Custodia='$Custodia1' WHERE ID_SQL=$idsql;";
     $resulupdate = mysqli_query($con2,$sqlUpdate) or die(mysqli_error($con2));
     if($resulupdate){
-        header("Location:gdl.php");
         echo "Perfecto eres un crack";
     }else {
         echo "ota vez te equivocaste por pendejo";
     }
     $con2->close();
+    header("Location:gdl.php");
 }
 
 
